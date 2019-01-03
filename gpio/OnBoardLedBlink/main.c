d7118b6eac2029669740e9043fde818edc9a9ba8
Gaurav Garg <33004490>
2019-01-03 22:30:52 +0530

    Led Toggling Started using Debug, Delay routine not working yet

--- gpio/OnBoardLedBlink/main.c
+++ gpio/OnBoardLedBlink/main.c
@@ -4,12 +4,23 @@
 /**
  * main.c
  */
+void delay2(long i)
+{
+    while(i!=0)
+        i--;
+}
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
 	P1DIR = 0b00000001;
-	P1OUT = 0b00000001;
+	while(1)
+	{
+	    P1OUT = 0b00000001;
+	    delay2(50000000);
+        P1OUT = 0b00000000;
+        delay2(50000000);
+	}
 
 	return 0;
 }
