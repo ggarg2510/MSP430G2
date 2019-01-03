c189749f96dac4c44059928cd211399ea4896f3f
Gaurav Garg <33004490>
2019-01-03 22:49:09 +0530

    P1.1 is blinking now

--- gpio/OnBoardLedBlink/main.c
+++ gpio/OnBoardLedBlink/main.c
@@ -16,10 +16,14 @@ int main(void)
 	P1DIR = 0b00000001;
 	while(1)
 	{
+	    int i=0;
 	    P1OUT = 0b00000001;
-	    delay2(50000000);
+	    for(i=0; i<30000; i++){if(i==5000) i = 5005;}
+
+//	    delay2(50000000);
         P1OUT = 0b00000000;
-        delay2(50000000);
+        for(i=0; i<30000; i++){if(i==5000) i = 5005;}
+  //      delay2(50000000);
 	}
 
 	return 0;
