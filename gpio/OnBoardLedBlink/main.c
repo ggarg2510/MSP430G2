23e0beeacb04287e853ba240e050bea3f5ed393f
Gaurav Garg <33004490>
2019-01-04 21:05:06 +0530

    Red-Green Blinking Alternatively

--- gpio/OnBoardLedBlink/main.c
+++ gpio/OnBoardLedBlink/main.c
@@ -13,7 +13,7 @@ int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
-	P1DIR = 0b00000001;
+	P1DIR = 0b01000001;
 	while(1)
 	{
 	    int i=0;
@@ -21,7 +21,7 @@ int main(void)
 	    for(i=0; i<30000; i++){if(i==5000) i = 5005;}
 
 //	    delay2(50000000);
-        P1OUT = 0b00000000;
+        P1OUT = 0b01000000;
         for(i=0; i<30000; i++){if(i==5000) i = 5005;}
   //      delay2(50000000);
 	}
